d5003b1d6b93639b1f0af32ab24142679448a5c4
DanDev_ <danuart21@gmail.com>
2025-03-27 21:53:07 +0800

    fix: setup pwa error

---
+++ next.config.js
@@ -0,0 +1,29 @@
+const withPWA = require('next-pwa')({
+  dest: 'public', // Output directory for service worker
+  register: true, // Automatically register service worker
+  skipWaiting: true, // Activate service worker immediately
+  scope: '/src/app',
+  sw: 'sw.js',
+  // disable: process.env.NODE_ENV === 'development', // Disable in development mode
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  images: {
+    domains: ["kckicxwbegihwljknvgk.supabase.co"], // Tambahkan domain supabase
+  },
+    /**
+   * @param {Configuration} config
+   * @returns {Configuration}
+   */
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+});
