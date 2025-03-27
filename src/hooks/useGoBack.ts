526fdad874efbaa2600a8e31dde28030e9228768
DanDev_ <danuart21@gmail.com>
2025-03-28 07:23:01 +0800

    feat: update useGoBack hooks

--- src/hooks/useGoBack.ts
+++ src/hooks/useGoBack.ts
@@ -4,11 +4,12 @@ const useGoBack = () => {
   const router = useRouter();
 
   const goBack = () => {
-    if (window.history.length > 1) {
-      router.back(); // Navigate to the previous route
-    } else {
-      router.push("/dashboard"); // Redirect to home if no history exists
-    }
+    router.push("/dashboard");
+    // if (window.history.length > 1) {
+    //   router.back(); // Navigate to the previous route
+    // } else {
+    //   router.push("/dashboard"); // Redirect to home if no history exists
+    // }
   };
 
   return goBack;
